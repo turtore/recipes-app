dcee5b0aa098aaead19212ade617306f65644076
thomasravache <thomasravache31@gmail.com>
2021-09-22 13:22:47 -0300

    Requisito 05 - OK

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, FloatingLabel, Container } from 'react-bootstrap';
 import './pages-css/LoginPage.css';
 
-function LoginPage() {
+const LoginPage = () => {
+  /* ------SETANDO ESTADOS--------- */
+  const [inputsState, setInputsState] = useState({
+    email: '',
+    password: '',
+  });
+  const [btnDisabledStatus, setBtnDisabledStatus] = useState(true);
+
+  /* ------FUNÇÃO QUE LIDA COM CADA MUDANÇA QUE HOUVER NOS INPUTS (EMAIL E SENHA)------*/
+  const handleChange = ({ target: { name, value } }) => {
+    setInputsState({
+      ...inputsState,
+      [name]: value,
+    });
+  };
+
+  /* useEffect utilizado para que toda vez que o inputsState for alterado, seja verificado se o
+    e-mail e password são validos ou não. Se forem validos, o botão é habilitado. */
+  useEffect(() => {
+    const emailVerification = /\S+@\S+\.\S+/;
+    const isValidEmail = emailVerification.test(inputsState.email);
+    const MAX_LENGTH = 6;
+    const isValidPassword = inputsState.password.length > MAX_LENGTH;
+
+    if (isValidEmail && isValidPassword) {
+      setBtnDisabledStatus(false);
+    } else {
+      setBtnDisabledStatus(true);
+    }
+  }, [inputsState]);
+
   return (
     <Container className="login-container">
       <h1 className="mb-4">Login</h1>
@@ -15,6 +45,9 @@ function LoginPage() {
           <Form.Control
             type="email"
             placeholder="name@example.com"
+            name="email"
+            onChange={ handleChange }
+            value={ inputsState.email }
             data-testid="email-input"
           />
         </FloatingLabel>
@@ -22,6 +55,9 @@ function LoginPage() {
           <Form.Control
             type="password"
             placeholder="Password"
+            name="password"
+            onChange={ handleChange }
+            value={ inputsState.password }
             data-testid="password-input"
           />
         </FloatingLabel>
@@ -30,6 +66,7 @@ function LoginPage() {
             variant="success"
             type="button"
             size="lg"
+            disabled={ btnDisabledStatus }
             data-testid="login-submit-btn"
           >
             Entrar
@@ -38,6 +75,6 @@ function LoginPage() {
       </Form>
     </Container>
   );
-}
+};
 
 export default LoginPage;
