92685ff305abbd90db273f99092f9ce39785d2cf
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-09-27 19:06:58 -0300

    Requisito 70

--- src/components/ExploreCocktail.jsx
+++ src/components/ExploreCocktail.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Button, Stack } from 'react-bootstrap';
 import Footer from './Footer';
+import Header from './Header';
 
 const ExploreCocktail = () => (
   <div>
+    <Header pageTitle="Explorar bebidas" showSearch={ false } />
+    <Stack gap={ 2 } className="col-md-5 mx-auto mt-4">
+      <Button
+        variant="secondary"
+        href="/explorar/bebidas/ingredientes"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        variant="secondary"
+        href="/explorar/bebidas/me-surpreenda"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </Button>
+    </Stack>
     <Footer />
   </div>
 );
--- src/components/ExploreMeal.jsx
+++ src/components/ExploreMeal.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Button, Stack } from 'react-bootstrap';
 import Footer from './Footer';
+import Header from './Header';
 
 const ExploreMeal = () => (
   <div>
+    <Header pageTitle="Explorar comidas" showSearch={ false } />
+    <Stack gap={ 2 } className="col-md-5 mx-auto mt-4">
+      <Button
+        variant="secondary"
+        href="/explorar/comidas/ingredientes"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        variant="secondary"
+        href="/explorar/comidas/origem"
+        data-testid="explore-by-area"
+      >
+        Por Origem
+      </Button>
+      <Button
+        variant="secondary"
+        href="/explorar/comidas/me-surpreenda"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </Button>
+    </Stack>
     <Footer />
   </div>
 );
