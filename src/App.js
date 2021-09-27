f81a1cdd338f1557764d52bb7e1db062a542fde7
thomasravache <thomasravache31@gmail.com>
2021-09-27 16:30:44 -0300

    Fix Req 54 ao 59

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import LoginPage from './pages/LoginPage';
 import DetailsRecipePage from './pages/DetailsRecipePage';
 import MealRecipePage from './pages/MealRecipePage';
 import CocktailRecipePage from './pages/CocktailRecipePage';
-// import ExplorePage from './pages/ExplorePage';
-// import DetailsMealRecipePage from './pages/DetailsMealRecipePage';
-// import ProfilePage from './pages/ProfilePage';
-// import MakeRecipesPage from './pages/MakeRecipesPage';
-// import FavoritesRecipesPage from './pages/FavoritesRecipesPage';
+import ExplorePage from './pages/ExplorePage';
+import ProfilePage from './pages/ProfilePage';
+import MakeRecipesPage from './pages/MakeRecipesPage';
+import FavoritesRecipesPage from './pages/FavoritesRecipesPage';
 // import SearchBar from './components/Searchbar';
 
 function App() {
@@ -25,6 +24,10 @@ function App() {
         path="/bebidas/:recipeId"
         component={ DetailsRecipePage }
       />
+      <Route exact path="/explorar" component={ ExplorePage } />
+      <Route exact path="/perfil" component={ ProfilePage } />
+      <Route exact path="/receitas-feitas" component={ MakeRecipesPage } />
+      <Route exact path="/receitas-favoritas" component={ FavoritesRecipesPage } />
       {/* <Route exact path="/comidas" component={ MealRecipesPage } />
       <Route exact path="/bebibas" component={ CocktailRecipesPage } />
       <Route exact path="/comidas/:id-da-receita" component={ DetailsMealRecipesPage } />
