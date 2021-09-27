8c0c6fb1a290292425a7f103570b88a8460353c5
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-09-27 20:38:37 -0300

    Requisito 74

--- src/components/ExploreCocktail.jsx
+++ src/components/ExploreCocktail.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Button, Stack } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import recipeAPI from '../services/recipeAPI';
 import Footer from './Footer';
 import Header from './Header';
 
-const ExploreCocktail = () => (
-  <div>
-    <Header pageTitle="Explorar bebidas" showSearch={ false } />
-    <Stack gap={ 2 } className="col-md-5 mx-auto mt-4">
-      <Button
-        variant="secondary"
-        href="/explorar/bebidas/ingredientes"
-        data-testid="explore-by-ingredient"
-      >
-        Por Ingredientes
-      </Button>
-      <Button
-        variant="secondary"
-        href="/explorar/bebidas/me-surpreenda"
-        data-testid="explore-surprise"
-      >
-        Me Surpreenda!
-      </Button>
-    </Stack>
-    <Footer />
-  </div>
-);
+const ExploreMeal = () => {
+  const history = useHistory();
 
-export default ExploreCocktail;
+  const handleClickRandomCockTail = async () => {
+    const dataDrink = await recipeAPI('randomRecipe', '', 'drink');
+    history.push(`/bebidas/${dataDrink.drinks[0].idDrink}`);
+  };
+
+  return (
+    <div>
+      <Header pageTitle="Explorar bebidas" showSearch={ false } />
+      <Stack gap={ 2 } className="col-md-5 mx-auto mt-4">
+        <Button
+          variant="secondary"
+          href="/explorar/bebidas/ingredientes"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </Button>
+        <Button
+          variant="secondary"
+          data-testid="explore-surprise"randomRecipe
+          onClick={ handleClickRandomCockTail }
+        >
+          Me Surpreenda!
+        </Button>
+      </Stack>
+      <Footer />
+    </div>
+  );
+};
+
+export default ExploreMeal;
